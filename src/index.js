e767f2af9c947174383e4159d02f083cba813ba9
Anthony Balmeo <anthony.balmeo@dollarshaveclub.com>
2018-10-02 13:35:36 -0700

    Add UIKIT

--- src/index.js
+++ src/index.js
@@ -3,6 +3,11 @@ import ReactDOM from 'react-dom'
 import './styles/index.css'
 import { App } from './components/App'
 import registerServiceWorker from './registerServiceWorker'
+import UIkit from 'uikit'
+import Icons from 'uikit/dist/js/uikit-icons'
+
+UIkit.use(Icons)
+// UIkit.notification('Hello world.')
 
 ReactDOM.render(<App />, document.getElementById('root'))
 registerServiceWorker()
